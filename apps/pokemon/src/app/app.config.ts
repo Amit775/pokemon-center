7af39a884a6d97868816a03f04429e0c44cc64c8
Amit775 <38419204+Amit775@users.noreply.github.com>
2024-06-23 15:06:50 +0300

    fix tests

--- apps/pokemon/src/app/app.config.ts
+++ apps/pokemon/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
 	provideExperimentalZonelessChangeDetection(),
-    provideRouter(appRoutes), 
+    provideRouter(routes), 
 	provideAnimationsAsync(),
   ],
 };
