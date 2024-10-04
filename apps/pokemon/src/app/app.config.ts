e65227ff72dba6e0f7abb145e69523ea5a6b44e7
Amit775 <38419204+Amit775@users.noreply.github.com>
2024-10-05 02:24:36 +0300

    commit

--- apps/pokemon/src/app/app.config.ts
+++ apps/pokemon/src/app/app.config.ts
@@ -1,25 +1,16 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import {
-  ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {
-  provideRouter,
-  withComponentInputBinding,
-  withInMemoryScrolling,
-} from '@angular/router';
+import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
+import { provideAngularQuery, QueryClient } from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideExperimentalZonelessChangeDetection(),
-    provideHttpClient(withFetch()),
-    provideRouter(
-      routes,
-      withComponentInputBinding(),
-      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }),
-    ),
-    provideAnimationsAsync(),
-  ],
+	providers: [
+		provideExperimentalZonelessChangeDetection(),
+		provideHttpClient(withFetch()),
+		provideRouter(routes, withComponentInputBinding(), withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })),
+		provideAnimationsAsync(),
+		provideAngularQuery(new QueryClient()),
+	],
 };
--- libs/data/src/lib/store/queryable-entities-feature.ts
+++ libs/data/src/lib/store/queryable-entities-feature.ts
@@ -3,50 +3,50 @@ import { withEntities } from '@ngrx/signals/entities';
 import { EmptyFeatureResult } from '@ngrx/signals/src/signal-store-models';
 
 export type QueryState = {
-  loading: boolean;
-  listOffsetIndex: number;
-  queryParams: { offset: number; limit: number };
+	loading: boolean;
+	listOffsetIndex: number;
+	queryParams: { offset: number; limit: number };
 };
 
 export type NamedQueryState<Collection extends string> = {
-  [Key in keyof QueryState as `${Collection}${Capitalize<Key>}`]: QueryState[Key];
+	[Key in keyof QueryState as `${Collection}${Capitalize<Key>}`]: QueryState[Key];
 };
 
 export type NamedQueryMethods<Collection extends string> = {
-  [Key in keyof QueryState as `update${Capitalize<Collection>}${Capitalize<Key>}`]: (value: QueryState[Key]) => void;
+	[Key in keyof QueryState as `update${Capitalize<Collection>}${Capitalize<Key>}`]: (value: QueryState[Key]) => void;
 };
 
 export type QueryStateKeys<Collection extends string> = {
-  [Key in keyof QueryState as `${Key}Key`]: keyof NamedQueryState<Collection> | keyof QueryState;
+	[Key in keyof QueryState as `${Key}Key`]: keyof NamedQueryState<Collection> | keyof QueryState;
 };
 
 const getQueryStateKeys = <Collection extends string>(config: { collection: Collection }): QueryStateKeys<Collection> => ({
-  loadingKey: `${config.collection}Loading`,
-  queryParamsKey: `${config.collection}QueryParams`,
-  listOffsetIndexKey: `${config.collection}ListOffsetIndex`,
+	loadingKey: `${config.collection}Loading`,
+	queryParamsKey: `${config.collection}QueryParams`,
+	listOffsetIndexKey: `${config.collection}ListOffsetIndex`,
 });
 
 const queryState = <Collection extends string>(config: { collection: Collection }): NamedQueryState<Collection> => {
-  const { loadingKey, queryParamsKey, listOffsetIndexKey } = getQueryStateKeys(config);
+	const { loadingKey, queryParamsKey, listOffsetIndexKey } = getQueryStateKeys(config);
 
-  return {
-    [loadingKey]: false,
-    [queryParamsKey]: { offset: 0, limit: 100 },
-    [listOffsetIndexKey]: 0,
-  } as NamedQueryState<Collection>;
+	return {
+		[loadingKey]: false,
+		[queryParamsKey]: { offset: 0, limit: 1000 },
+		[listOffsetIndexKey]: 0,
+	} as NamedQueryState<Collection>;
 };
 
 export const withQueryFeature = <Entity, Collection extends string>(config: {
-  entity: Entity;
-  collection: Collection;
+	entity: Entity;
+	collection: Collection;
 }): SignalStoreFeature<
-  EmptyFeatureResult,
-  {
-    state: NamedQueryState<Collection>;
-    methods: Record<string, never>;
-    computed: Record<string, never>;
-  }
+	EmptyFeatureResult,
+	{
+		state: NamedQueryState<Collection>;
+		methods: Record<string, never>;
+		computed: Record<string, never>;
+	}
 > => signalStoreFeature(withState(queryState(config)));
 
 export const withQueryableEntitiesFeature = <Entity, Collection extends string>(config: { entity: Entity; collection: Collection }) =>
-  signalStoreFeature(withEntities(config), withQueryFeature(config));
+	signalStoreFeature(withEntities(config), withQueryFeature(config));
