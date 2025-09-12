895b4bc5877012addafa033d2de73d9cc8bf8dc6
Amit775 <38419204+Amit775@users.noreply.github.com>
2025-09-13 01:33:46 +0300

    commit

--- libs/infra-pokedex-data/src/entities/models/Moves.ts
+++ libs/infra-pokedex-data/src/entities/models/Moves.ts
@@ -1,140 +1,137 @@
-import * as TypeGraphQL from "type-graphql";
-import * as GraphQLScalars from "graphql-scalars";
-import { Prisma } from "@prisma/client";
-import { DecimalJSScalar } from "../scalars";
-import { ContestCombos } from "../models/ContestCombos";
-import { ContestEffects } from "../models/ContestEffects";
-import { ContestTypes } from "../models/ContestTypes";
-import { Generations } from "../models/Generations";
-import { Machines } from "../models/Machines";
-import { MoveDamageClasses } from "../models/MoveDamageClasses";
-import { MoveEffects } from "../models/MoveEffects";
-import { MoveFlagMap } from "../models/MoveFlagMap";
-import { MoveMeta } from "../models/MoveMeta";
-import { MoveMetaStatChanges } from "../models/MoveMetaStatChanges";
-import { MoveTargets } from "../models/MoveTargets";
-import { PokemonEvolution } from "../models/PokemonEvolution";
-import { PokemonMoves } from "../models/PokemonMoves";
-import { SuperContestCombos } from "../models/SuperContestCombos";
-import { SuperContestEffects } from "../models/SuperContestEffects";
-import { Types } from "../models/Types";
-import { MovesCount } from "../resolvers/outputs/MovesCount";
+import * as TypeGraphQL from 'type-graphql';
+import { ContestCombos } from '../models/ContestCombos';
+import { ContestEffects } from '../models/ContestEffects';
+import { ContestTypes } from '../models/ContestTypes';
+import { Generations } from '../models/Generations';
+import { Machines } from '../models/Machines';
+import { MoveDamageClasses } from '../models/MoveDamageClasses';
+import { MoveEffects } from '../models/MoveEffects';
+import { MoveFlagMap } from '../models/MoveFlagMap';
+import { MoveMeta } from '../models/MoveMeta';
+import { MoveMetaStatChanges } from '../models/MoveMetaStatChanges';
+import { MoveTargets } from '../models/MoveTargets';
+import { PokemonEvolution } from '../models/PokemonEvolution';
+import { PokemonMoves } from '../models/PokemonMoves';
+import { SuperContestCombos } from '../models/SuperContestCombos';
+import { SuperContestEffects } from '../models/SuperContestEffects';
+import { Types } from '../models/Types';
+import { MovesCount } from '../resolvers/outputs/MovesCount';
 
-@TypeGraphQL.ObjectType("Moves", {})
+@TypeGraphQL.ObjectType('Moves', {})
 export class Moves {
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: false
-  })
-  id!: number;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: false,
+	})
+	id!: number;
 
-  @TypeGraphQL.Field(_type => String, {
-    nullable: false
-  })
-  identifier!: string;
+	@TypeGraphQL.Field((_type) => String, {
+		nullable: false,
+	})
+	identifier!: string;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: false
-  })
-  generation_id!: number;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: false,
+	})
+	generation_id!: number;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: false
-  })
-  type_id!: number;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: false,
+	})
+	type_id!: number;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: true
-  })
-  power?: number | null;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: true,
+	})
+	power?: number | null;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: true
-  })
-  pp?: number | null;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: true,
+	})
+	pp?: number | null;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: true
-  })
-  accuracy?: number | null;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: true,
+	})
+	accuracy?: number | null;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: false
-  })
-  priority!: number;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: false,
+	})
+	priority!: number;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: false
-  })
-  target_id!: number;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: false,
+	})
+	target_id!: number;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: false
-  })
-  damage_class_id!: number;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: false,
+	})
+	damage_class_id!: number;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: true
-  })
-  effect_id?: number | null;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: true,
+	})
+	effect_id?: number | null;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: true
-  })
-  effect_chance?: number | null;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: true,
+	})
+	effect_chance?: number | null;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: true
-  })
-  contest_type_id?: number | null;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: true,
+	})
+	contest_type_id?: number | null;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: true
-  })
-  contest_effect_id?: number | null;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: true,
+	})
+	contest_effect_id?: number | null;
 
-  @TypeGraphQL.Field(_type => TypeGraphQL.Int, {
-    nullable: true
-  })
-  super_contest_effect_id?: number | null;
+	@TypeGraphQL.Field((_type) => TypeGraphQL.Int, {
+		nullable: true,
+	})
+	super_contest_effect_id?: number | null;
 
-  generation?: Generations;
+	generation?: Generations;
 
-  type?: Types;
+	type?: Types;
 
-  target?: MoveTargets;
+	target?: MoveTargets;
 
-  damageClass?: MoveDamageClasses;
+	damageClass?: MoveDamageClasses;
 
-  effect?: MoveEffects | null;
+	effect?: MoveEffects | null;
 
-  contestType?: ContestTypes | null;
+	contestType?: ContestTypes | null;
 
-  contestEffect?: ContestEffects | null;
+	contestEffect?: ContestEffects | null;
 
-  superContestEffect?: SuperContestEffects | null;
+	superContestEffect?: SuperContestEffects | null;
 
-  pokemonMoves?: PokemonMoves[];
+	pokemonMoves?: PokemonMoves[];
 
-  machines?: Machines[];
+	machines?: Machines[];
 
-  meta?: MoveMeta[];
+	meta?: MoveMeta[];
 
-  metaStatChanges?: MoveMetaStatChanges[];
+	metaStatChanges?: MoveMetaStatChanges[];
 
-  flagMap?: MoveFlagMap[];
+	flagMap?: MoveFlagMap[];
 
-  contestCombos?: ContestCombos[];
+	contestCombos?: ContestCombos[];
 
-  contestCombosSecond?: ContestCombos[];
+	contestCombosSecond?: ContestCombos[];
 
-  superContestCombos?: SuperContestCombos[];
+	superContestCombos?: SuperContestCombos[];
 
-  superContestCombosSecond?: SuperContestCombos[];
+	superContestCombosSecond?: SuperContestCombos[];
 
-  knownMoves?: PokemonEvolution[];
+	knownMoves?: PokemonEvolution[];
 
-  @TypeGraphQL.Field(_type => MovesCount, {
-    nullable: true
-  })
-  _count?: MovesCount | null;
+	@TypeGraphQL.Field((_type) => MovesCount, {
+		nullable: true,
+	})
+	_count?: MovesCount | null;
 }
